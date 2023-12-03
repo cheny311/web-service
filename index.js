733eb6b2aca590587ae0104ad3e410e140c55fd2
Jessie Chen <112643063+cheny311@users.noreply.github.com>
2023-12-03 00:18:23 -0500

    path

--- index.js
+++ index.js
@@ -22,43 +22,43 @@ let phenomenon = {
   0: {
     name: "Light Pillar",
     info: "An optical illusion, a light pillar, is formed when a beam of light appears to be extended above or below a light source. The effect is seen when light is reflected from the crystals of ice in the atmosphere. The reflection from the sun is called Solar Pillar. Seeing this natural phenomenon feels like you are in between the colourful waterfalls. ",
-    image: "../images/Light-pillar.jpeg",
-    audio: "../music/lightpillar.mp3",
+    image: "images/Light-pillar.jpeg",
+    audio: "music/lightpillar.mp3",
   },
 
   1: {
     name: "Aurora",
     info: "Most of the solar wind is blocked by the magnetosphere however some of the ions briefly get trapped in ring-shaped holding areas around the planet. These areas are in a region of the atmosphere called the ionosphere. In the ionosphere, the ions of the solar wind  collide with atoms of oxygen and nitrogen from Earth’s atmosphere. The energy released during these collisions causes a colorful glowing halo around the poles—an aurora. Auroras are also known around the world as polar lights, northern lights or southern lights.",
-    image: "../images/aurora1.jpeg",
-    audio:  "../music/aurora.mp3",
+    image: "images/aurora1.jpeg",
+    audio:  "music/aurora.mp3",
   },
 
   2: {
     name: "Nacreous",
     info: "Nacreous Clouds, which are also known as polar stratospheric clouds, are usually spotted in the Polar Regions, high up in the atmosphere, where the air is cold and dry. The setting sun lies lower than the clouds, creating a colorful iridescent shine as they reflect sunbeams back. These clouds are also known as the ‘mother of pearl clouds’ and are only seen at the twilight hours of dawn and dusk. But, despite their beauty, they actually harm the ozone layer.",
-    image: "../images/nacreousclouds.jpeg",
-    audio: "../music/nacreousclouds.mp3",
+    image: "images/nacreousclouds.jpeg",
+    audio: "music/nacreousclouds.mp3",
   },
 
   3: {
     name: "Blue Lava",
     info: "Kawah Ijen in Indonesia also nicknamed 'The Blue Volcano' erupts lava that appears blue due to the combustion of sulphuric gases in contact with air at temperatures above 360°C. ",
-    image: "../images/bluelava.jpeg",
-    audio: "../music/bluelava.mp3",
+    image: "images/bluelava.jpeg",
+    audio: "music/bluelava.mp3",
   },
 
   4: {
     name: "Lenticular",
     info: "Lenticular clouds are stationary lens-shaped clouds that form at high altitudes, especially over mountains and even buildings. The wind flow is obstructed by these tall structures, condensing to form the unique clouds that are often confused for UFOs. They occur most commonly in the winter and the spring.",
-    image: "../images/lenticularclouds.jpeg",
-    audio: "../music/lenticularclouds.mp3",
+    image: "images/lenticularclouds.jpeg",
+    audio: "music/lenticularclouds.mp3",
   },
 
   5: {
     name: "Catatumbo",
     info: "Catatumbo lightning is an atmospheric phenomenon that occurs over the mouth of the Catatumbo River where it empties into lake Maracaibo in Venezuela. Catatumbo means 'House of Thunder' in the language of the Bari people. It originates from a mass of storm clouds at an altitude of more than 1 km (0.6 mi), and occurs for 140 to 160 nights a year, nine hours per day, and with lightning flashes from 16 to 40 times per minute.The phenomenon sees the highest density of lightning in the world, at 250 per km2",
-    image: "../images/catatumbo.jpeg",
-    audio: "../music/catatumbo.mp3",
+    image: "images/catatumbo.jpeg",
+    audio: "music/catatumbo.mp3",
   },
 };
 
